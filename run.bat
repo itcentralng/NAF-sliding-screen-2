d2ee179a2d94684be86e6281605523a4827cee64
Nasir Mustapha <teboy.com@gmail.com>
2025-09-07 16:26:26 +0100

    Add finance specialty logo and update welcome page content for NAF Finance Specialty

--- run.bat
+++ run.bat
@@ -5,4 +5,5 @@ start /MIN "" python main.py
 timeout /t 5 /nobreak
 
 :: Launch Edge in fullscreen kiosk mode
-start "" "msedge" --kiosk --edge-kiosk-type=fullscreen --no-first-run --disable-features=msEdgeEnterpriseModePolicies http://127.0.0.1:8000/welcome.html
+:: start "" "msedge" --kiosk --edge-kiosk-type=fullscreen --no-first-run --disable-features=msEdgeEnterpriseModePolicies http://127.0.0.1:8000/welcome.html
+start "" "msedge" --app=http://127.0.0.1:8000/welcome.html
